ba932f32158b42f4d733537f4c0c083c2298674b
Tyisha Williams <tlwilliams895@gmail.com>
2020-06-25 11:40:53 -0400

    assessment ready

--- src/hanoi.js
+++ src/hanoi.js
@@ -37,10 +37,27 @@ let selectedDisc = document.querySelector("#currentBlock");
 
 // Start and End Game Button (Main Functions) -- JS for gameDetails class (Game Controls Bar)
 const startBtn = document.getElementById("#startBtn");
+//let col_two_details = document.querySelector(".col_two_details");
+//let col_three_details = document.querySelector(".col_three_details");
 //startBtn.addEventListener("click", startGame);
 
 // Learned from Randy in Demo and helped by Nikal Morgan (SE Coach)
 const towerClick = function (event) {
+    // tower_container.style.visibility = "visible";
+    // col_two_details.style.visibility = "visible";
+    // col_three_details.style.visibility = "visible";
+
+    // if (startBtn.textContent === "End Game") {
+    //     startBtn.textContent === "Start Game";
+
+    //     tower_container.style.visibility = "hidden";
+    //     // The reload() method is used to reload the current document; same as the reload button in your browser.
+    //     location.reload();
+    //     return;
+    // } else {
+    //     startBtn.textContent = "End Game";
+    // }
+
     const towerOfHanoi = event.currentTarget;
 
     if (gameMode === "pickUp") {
@@ -85,9 +102,13 @@ const towerClick = function (event) {
             }
         }
     }
+    // If-Statement for Winning Condition - If all four discs are inside the third tower, the game is over. 
     if (tower3.childElementCount === 4) {
         setTimeout(alert(`Congratulations! You won with ${moveCount} moves!`), 500);
     }
+    // Use setTimeout Method to call the towerPlayerWin function to initialize the timer 
+    // at 500 milliseconds to alert the player has won the game. 
+    // Note to Self: Practice more Template Literals
 }
 
 // Moves Counter Functions
@@ -96,122 +117,15 @@ let moveCount_span = document.querySelector('.counter')
 for (let i = 0; i < towers.length; i++) {
     towers[i].addEventListener('click', towerClick)
 }
-// let counter = 0;
-// let counterDashboard = document.querySelector(".counter");
-// let col_two_details = document.querySelector(".col_two_details");
-// let col_three_details = document.querySelector(".col_three_details");
 
-// Selected Discs Functions (must function/execute with Tower and Disc Game Functions)
-// let selectedDisc = document.querySelector("#currentBlock");
-
-// const displayDiscMove = function () {
-//     while (selectedDisc.firstChild) {
-//         selectedDisc.removeChild(selectedDisc.firstChild);
-//     }
-//     if (currentDisc) {
-//         let discClone = currentDisc.cloneNode(true);
-//         selectedDisc.appendChild(discClone);
-//     } else {
-//         selectedDisc.innerHTML = "--- Selected Disc ---";
-//     }
-//     counterDashboard.innerText = counter;
-// }
-
-// for (let i = 1; i <= 3; i++) {
-//     towersArray.push(document.querySelector(`.tower${i}`));
-//     //console.log(towersArray) -- Issue right here
-// }
-
-// // Clear Game Function (using Start/End Button to clear game board)
+// Clear Game Function (using Start/End Button to clear game board)
 // function clearGameBoard(towerOfHanoi) {
 //     while (towerOfHanoi.firstChild) {
 //         towerOfHanoi.removeChild(towerOfHanoi.firstChild);
 //     }
 // }
 
-
-// function startGame() {
-//     tower_container.style.visibility = "visible";
-//     col_two_details.style.visibility = "visible";
-//     col_three_details.style.visibility = "visible";
-
-//     if (startBtn.textContent === "End Game") {
-//         startBtn.textContent === "Start Game";
-
-//         tower_container.style.visibility = "hidden";
-//         // The reload() method is used to reload the current document; same as the reload button in your browser.
-//         location.reload();
-//         return;
-//     } else {
-//         startBtn.textContent = "End Game";
-//     }
-
-// The map() method creates a new array populated with the results of 
-// calling a provided function on every element in the calling array (const towers). MDN Reference Used as help!
-
-//towersArray.map(clearGameBoard);
-//currentDisc = null;
-// counter = 0;
-// displayDiscMove();
-
-// Winning Condition - If all four discs are inside the third tower, the game is over. 
-// if (tower3.childElementCount === 4)
-// setTimeout(alert(`Congratulations! You won with ${counter} moves!`), 500);
-
-// function towerPlayerWin() {
-//     return towersArray[0].childNodes.length === 0 && towersArray[1].childNodes.length === 0;
-// }
-
-
-// function towerMovement() {
-
-//     displayDiscMove();
-
-// If-Statement for Winning Condition
-// Use setTimeout Method to call the towerPlayerWin function to initialize the timer 
-// at 500 milliseconds to alert the player has won the game. 
-// if (towerPlayerWin()) {
-//     setTimeout(alert(`Congratulations! You won with ${counter} moves!`), 500);
-// }
-// Note to Self: Practice more Template Literals
-
-// Click event to execute the towerMovement function
-// towersArray.map(function (values) {
-//     values.addEventListener("click", function (event) {
-//         towerMovement(event.target);
-//     })
-//})
-//     towerMovement();
-// }
-//}
-
-
-
-// Trying to figure out how to use the array
-// function towerMovement(towerOfHanoi) {
-//     if (!currentDisc) {
-//         currentDisc = getLastDisc(towerOfHanoi);
-//     } else if (currentDisc && towerOfHanoi.childNodes.length === 0) {
-
-//         towerOfHanoi.appendChild(currentDisc);
-//         counter++;
-//         currentDisc = null;
-
-//     } else if (currentDisc) {
-//         let currentDiscWidth = parseInt(currentDisc.style.width);
-//         let topDiscWidth = parseInt(getLastDisc(towerOfHanoi).style.width);
-
-//         if (currentDiscWidth <= topDiscWidth) {
-//             towerOfHanoi.appendChild(currentDisc);
-//             counter++;
-//             currentDisc = null;
-//         }
-//     }
-// }
-
-
-
-
+//--------------------------------------------------------------------------------------------
 // DRY - DON'T REPEAT YOURSELF!! LOL!!
 // tower1.addEventListener("click", function (event) {
 //     activeDisc = !activeDisc;
